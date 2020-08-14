68e77924027e6ae71210ad3347c50730702124c9
Kristina Zhizherina <diablito.yey@mail.ru>
2020-08-14 16:36:06 +0200

    controllers work

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :cocktails do
+  root to: 'cocktails#index'
+
+  resources :cocktails, only: [ :index, :new, :create, :show ] do
     resources :doses, only: [ :new, :create ]
   end
+
+  resources :doses, only: [ :destroy ]
+
 end
